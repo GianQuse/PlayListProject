10f35187aed3df0faf81289f68e28740787664fb
GianQuse <107727901+GianQuse@users.noreply.github.com>
2023-02-07 18:58:05 -0300

    Cargar canciones

--- js/main.js
+++ js/main.js
@@ -23,12 +23,21 @@ const songs = document.getElementById("songs")
 
 //CARGAR CANCIONES Y MOSTRAR LISTADO.-
 function loadSongs() {
-    songList.forEach(song => {
+    songList.forEach((song, index) => {
         const li = document.createElement("li");
         const link = document.createElement("a");
         link.textContent = song.title;
         link.href = "#";
+        link.addEventListener("click", () => loadSongs(index))
         li.appendChild(link);
         songs.appendChild(li);
     })
 }
+
+//CARGAR CANCION SELECCIONADA.-
+function loadSong(songIndex) {
+    
+}
+
+//INICIOS FUNCIONES.-
+loadSongs()
