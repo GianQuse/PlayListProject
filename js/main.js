b0e45e6921ee3000aa6063ed1e1adde08c79c581
GianQuse <107727901+GianQuse@users.noreply.github.com>
2023-02-07 18:39:02 -0300

    Lista Canciones

--- js/main.js
+++ js/main.js
@@ -17,3 +17,18 @@ const songList = [
         cover: "#"
     },
 ]
+
+//CAPTURAR ELEMENTOS DEL DOM.-
+const songs = document.getElementById("songs")
+
+//CARGAR CANCIONES Y MOSTRAR LISTADO.-
+function loadSongs() {
+    songList.forEach(song => {
+        const li = document.createElement("li");
+        const link = document.createElement("a");
+        link.textContent = song.title;
+        link.href = "#";
+        li.appendChild(link);
+        songs.appendChild(li);
+    })
+}
